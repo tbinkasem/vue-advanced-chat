9bc102be54e191f60de6b8f91cdc834e48d15f95
antoine92190 <dupontantoine92@gmail.com>
2020-02-09 11:54:26 +1100

    (test) add vue

--- src/ChatWindow/index.js
+++ src/ChatWindow/index.js
@@ -1,16 +1,19 @@
-import ChatWindow from './ChatWindow'
+// import ChatWindow from './ChatWindow'
+// import vueCustomElement from 'vue-custom-element'
+
+// Object.defineProperty(ChatWindow, 'install', {
+// 	configurable: false,
+// 	enumerable: false,
+// 	value(Vue) {
+// 		Vue.component('chat-window', ChatWindow)
+// 	}
+// })
+
+// export default ChatWindow
+
+import Vue from 'vue'
 import vueCustomElement from 'vue-custom-element'
+import ChatWindow from './ChatWindow'
 
-Object.defineProperty(ChatWindow, 'install', {
-	configurable: false,
-	enumerable: false,
-	value(Vue) {
-		// Vue.customElement('chat-window', ChatWindow);
-		// Vue.component('chat-window', ChatWindow)
-
-		Vue.use(vueCustomElement)
-		Vue.customElement('chat-window', ChatWindow)
-	}
-})
-
-export default ChatWindow
+Vue.use(vueCustomElement)
+Vue.customElement('chat-window', ChatWindow)
