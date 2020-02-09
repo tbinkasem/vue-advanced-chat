68bad8a9320bfb5e275d3b208f86101828f97b48
antoine92190 <dupontantoine92@gmail.com>
2020-02-09 13:20:15 +1100

    fix

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
-// import vueCustomElement from 'vue-custom-element'
+import vueCustomElement from 'vue-custom-element'
 import ChatWindow from './ChatWindow/ChatWindow'
 
-Vue.component('chat-window', ChatWindow)
-
-// Vue.use(vueCustomElement)
-// Vue.customElement('chat-window', ChatWindow)
+Vue.use(vueCustomElement)
+Vue.customElement('chat-window', ChatWindow)
