45c765fd2e40e9a3eef4da2cc6d8aa068215f125
antoine92190 <dupontantoine92@gmail.com>
2020-02-09 11:33:33 +1100

    (test) use vue-custom-element

--- src/ChatWindow/index.js
+++ src/ChatWindow/index.js
@@ -1,11 +1,15 @@
 import ChatWindow from './ChatWindow'
+import vueCustomElement from 'vue-custom-element'
 
 Object.defineProperty(ChatWindow, 'install', {
 	configurable: false,
 	enumerable: false,
 	value(Vue) {
-		Vue.customElement('chat-window', ChatWindow);
+		// Vue.customElement('chat-window', ChatWindow);
 		// Vue.component('chat-window', ChatWindow)
+
+		Vue.use(vueCustomElement)
+		Vue.customElement('chat-window', ChatWindow)
 	}
 })
 
